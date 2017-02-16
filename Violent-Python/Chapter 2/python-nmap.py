d4f34af8339ea0203e73c16855b54061f8156933
Lantianor <lantianor@126.com>
2017-02-16 10:57:20 +0800

    Update python-nmap.py

--- Violent-Python/Chapter 2/python-nmap.py
+++ Violent-Python/Chapter 2/python-nmap.py
@@ -5,7 +5,6 @@ def nmapScan(tgtHost, tgtPort):
 	nmScan.scan(tgtHost,tgtPort)
 	state=nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
 	print "[*] " + tgtHost + " tcp/" + tgtPort + " " + state
-
 def main() :
 	parser = optparse.OptionParser('usage%prog '+ '-H <target host> -p <target port>')
 	parser.add_option('-H', dest='tgtHost', type='string', help='specify target port[s] separated by comma')
